c4bfb3d82da2251f5f1a8a1ed35685b692fac780
Rita <rituzka@gmail.com>
2020-11-15 21:37:05 +0100

    refactoring model classes

--- src/main/java/com/itAcademy/ex14diceplayer/model/Game.java
+++ src/main/java/com/itAcademy/ex14diceplayer/model/Game.java
@@ -1,52 +1,77 @@
 package com.itAcademy.ex14diceplayer.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
 
+
+@Entity
+@Table(name = "games")
 public class Game extends AbstractEntity implements Serializable {
 
-    private Player player;
-    private int dice1;
-    private int dice2;
-    private boolean winner;
+    @Column(name = "game_result")
+    @NotNull
+    private int result;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "player_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    public Player player;
+
+
+    //constructor #1 with parameter
+    public Game(Player player) {
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+        this.result = dice1 + dice2;
+        boolean winner = winGame(result);
+        this.player = player;
+    }
+    //constructor #2 without parameters
+    public Game(){}
+
+
+    //getters & setters
+    public int getDice1() {
+        return rollDice();
+    }
+
+    public int getDice2() {
+        return rollDice();
+    }
+
+    public int getResult() {
+        return getDice1()+getDice2();
+    }
+
+    public boolean isWinner() {
+        return winGame(getResult());
+    }
 
     public Player getPlayer() {
         return player;
     }
 
-    public void setPlayer(Player player) {
-        this.player = player;
+    //roll the dice, random number
+    public int rollDice(){
+        Random r = new Random();
+              return r.nextInt(6)+1;
     }
 
-    public int getDice1() {
-        return dice1;
+    public boolean winGame (int result) {
+     this.result = result;
+        return result == 7;
+    }
+    
+    public double successAverage(List<Integer> results){
+        return results.stream().mapToDouble(result -> result).average().getAsDouble();
     }
 
-    public void setDice1(int dice1) {
-        this.dice1 = dice1;
-    }
 
-    public int getDice2() {
-        return dice2;
-    }
-
-    public void setDice2(int dice2) {
-        this.dice2 = dice2;
-    }
-
-    public boolean isWinner() {
-        return winner;
-    }
-
-    public void setWinner(boolean winner) {
-        this.winner = winner;
-    }
-
-    public Game(Player player, int dice1, int dice2, boolean winner) {
-        this.player = player;
-        this.dice1 = dice1;
-        this.dice2 = dice2;
-        this.winner = winner;
-
-
-    }
 }
--- src/main/java/com/itAcademy/ex14diceplayer/model/Player.java
+++ src/main/java/com/itAcademy/ex14diceplayer/model/Player.java
@@ -1,12 +1,38 @@
 package com.itAcademy.ex14diceplayer.model;
 
 
-import java.io.Serializable;
 
+import com.sun.istack.NotNull;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "players")
 public class Player extends AbstractEntity implements Serializable {
 
-
+    @Column(name = "player_username")
     private String username;
+
+    @Column(name = "player_registration")
+    @NotNull
+    private Date registrationDate;
+
+    public Player(String username, Date registrationDate) {
+        this.username = username;
+        this.registrationDate = registrationDate;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
 
     public Player(String username) {
         this.username = username;
@@ -19,4 +45,5 @@ public class Player extends AbstractEntity implements Serializable {
     public void setUsername(String username) {
         this.username = username;
     }
+
 }
